2dacd3ffbfd6e2e6440194f6684a993b1186648e
OnurMorcagok <onurmorcagok@gmail.com>
2020-06-19 23:45:04 +0300

    Update app.js

--- ES6+ Inheritance/app.js
+++ ES6+ Inheritance/app.js
@@ -1,6 +1,6 @@
-class Person {
-    constructor(name, age) {
+class Person { // Superclass, Baseclass
 
+    constructor(name, age) {
         this.name = name;
         this.age = age;
     }
@@ -10,28 +10,31 @@ class Person {
     }
 }
 
-class Employee extends Person {
-
+class Employee extends Person { // Subclass, DerivedClass, ChildClass
     constructor(name, age, salary) {
-
+        // this.name = name;
+        // this.age = age;
         super(name, age);
         this.salary = salary;
     }
 
-    showInfos() { //Overriding
-        console.log("Name : " + this.name + " Age: " + this.age + " Salary: " + this.age);
+    showInfos() { // Overriding
+        console.log("Name:" + this.name + " Age:" + this.age + " Salary:" + this.salary);
     }
 
     toString() {
         console.log("toString method called");
     }
 
-    raiseSalary(amount){ //Extra Method
+    raiseSalary(amount) { // Extra Method
         this.salary += amount;
     }
 }
 
 const emp = new Employee("Onur M.", 23, 5000);
+
+emp.raiseSalary(2000);
+emp.showInfos();
 
 // console.log(emp);
 // console.log(toString);
@@ -39,4 +42,3 @@ const emp = new Employee("Onur M.", 23, 5000);
 // emp.showInfos();
 // emp.toString();
 
-emp.raiseSalary(5000);
